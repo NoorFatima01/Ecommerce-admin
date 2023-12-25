d29165cd02c8955f174eb574363c3b4f3f865c49
Noor Fatima <nfatima101204@gmail.com>
2023-12-25 11:32:26 +0500

    Added authentication (nextAuth) and connected to monogdb database

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import '../styles/globals.css'
+import  AuthSessionProvider  from '@/lib/AuthSessionProvider'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthSessionProvider>
+      <body>{children}</body>
+
+      </AuthSessionProvider>
     </html>
   )
 }
+
